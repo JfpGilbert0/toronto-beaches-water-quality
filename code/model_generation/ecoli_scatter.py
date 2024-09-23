5b50a5e008f8670b34bb3673193e596ee9794042
JGilbert <j.gilbert@mail.utoronto.ca>
2024-09-23 12:49:20 -0400

    converts scatter plot to log

--- code/model_generation/ecoli_scatter.py
+++ code/model_generation/ecoli_scatter.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import MaxNLocator
 
-plt.ion()
 # Import data
-data = pd.read_csv("../data/cleaned_water_quality_data.csv")
+data = pd.read_csv("data/cleaned_water_quality_data.csv")
 df = pd.DataFrame(data)
 
 # generate graph
@@ -18,19 +17,22 @@ df['log_ecoli'] = np.log(df['ecoli'])
 sns.set(style="whitegrid", context="talk")
 
 # Create a scater plot
-sns.scatterplot(data=df, x='collectiondate', y='ecoli', hue='beachname', palette='Set1', s=20, alpha=0.7)
+sns.scatterplot(data=df, x='collectiondate', y='log_ecoli', hue='beachname', palette='Set1', s=20, alpha=0.7)
 
 # Set title and labels
 plt.title('E. coli Levels Over Time by Site and Beach', fontsize=16)
 plt.xlabel('Collection Date', fontsize=14)
-plt.ylabel('E. coli Level (MPN/100mL)', fontsize=14)
+plt.ylabel('E. coli Level (Log(MPN/100mL))', fontsize=14)
 plt.grid(True, axis='x')
 # modify x axis labels
-plt.xticks(df['collectiondate'][::100], rotation=45)  
-plt.tight_layout() 
+plt.xticks(df['collectiondate'], fontsize=10, rotation=75 )  
+plt.yticks(df['log_ecoli'], fontsize=10)
+plt.tight_layout()
 
-plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=10))
-plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%Y %M'))
+plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=17))
+plt.gca().yaxis.set_major_locator(MaxNLocator(nbins=10))
+
+plt.gca().xaxis.set_major_formatter(plt.matplotlib.dates.DateFormatter('%b %Y'))
 plt.legend(title='Beach Name', loc='upper left', bbox_to_anchor=(1.05, 1))
 
 
